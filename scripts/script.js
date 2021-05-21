ff1dd70d7098960376b8ae117724b57237fe7105
Lucas Carvalho <lucas.crvlh.s@gmail.com>
2021-05-20 23:18:24 -0300

    adding the second request and display of table in the page

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,16 @@
 let form = document.getElementsByTagName("form")[0];
-let results = document.getElementsByClassName("results")[0];
 let button = document.getElementsByTagName("button")[0];
+let results = document.getElementsByClassName("results")[0];
 
-const record_search = 'http://localhost:5000/record_search'
 
+const record_search_url = 'http://localhost:5000/record_search'
+let get_jobs = 'http://localhost:5000/get_jobs/'
 
 
 // https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms/Sending_forms_through_JavaScript
 function sendData() {
-  console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-  var XHR = new XMLHttpRequest();
+  // console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+  // var XHR = new XMLHttpRequest();
   let search = {}
 
   // radio buttons
@@ -29,14 +30,43 @@ function sendData() {
     }
   }
 
-  fetch(record_search, {
-    method: form.getAttribute("method"),body: JSON.stringify(search)})
+  // fetch(record_search_url, {
+  //   method: 'POST',body: JSON.stringify(search)})
+  //     .then(function(response) {return response.json();})
+  //     .then(function(data) {
+  //       console.log("Data returned from python server", data)
+  // });
+
+
+  let addStyleToResults = () => {
+    results.style.borderRadius = '10px';
+    results.style.boxShadow ='0 0 15px 4px rgba(0,0,0,0.06)';
+    results.style.marginBottom = '2%';
+  }
+
+  let jobsValidation = () => {
+    if (document.getElementsByTagName("tr").length === 0) {
+      results.innerHTML = "<div class='nojobs' > There aren't jobs for this stack in this city... </div>"
+    }
+  }
+
+ 
+
+  get_jobs_url = get_jobs.concat(search["location"], '/', search["description"])
+
+
+
+  fetch(get_jobs_url, { method:'GET'})
       .then(function(response) {return response.json();})
-      .then(function(data) {
-        console.log("Data returned from python server", data)
+      .then(function(jobs_data) {
+        console.log(jobs_data);
+        addStyleToResults();
+        results.innerHTML= jobs_data;
+        jobsValidation();
   });
 
-  // let response = fetch(record_search, {
+
+  // let response = fetch(record_search_url, {
   //   method: "POST",
   //   headers: {'Content-Type': 'application/json'},
   //   body: JSON.stringify(search),
@@ -61,7 +91,7 @@ function sendData() {
   // });
 
   // Set up our request
-  // XHR.open(form.getAttribute("method"), record_search);
+  // XHR.open(form.getAttribute("method"), record_search_url);
   // XHR.setRequestHeader("Content-Type", "application/json");
   // XHR.send(JSON.stringify(search));
 
